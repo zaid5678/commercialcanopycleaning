e20dbb96df62c11d292799bae6cd9a42aff687b0
Zaid <zfkhan321@gmail.com>
2026-04-10 02:37:14 +0100

    Navbar: increase logo size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
               <img
                 src="/logo-updated.jpeg"
                 alt="Commercial Canopy Cleaning"
-                className="h-12 sm:h-14 lg:h-16 w-auto object-contain rounded-sm"
+                className="h-14 sm:h-16 lg:h-20 w-auto object-contain rounded-sm"
               />
             </Link>
 
